6fedebe3b1dd758c72d5a11804bf226e4152b839
HongJay <zjxuzhj@gmail.com>
2017-09-06 21:57:25 +0800

    Initial commit

---
+++ app/src/main/java/zhj/meizigank/MainActivity.java
@@ -0,0 +1,13 @@
+package zhj.meizigank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//    private RecyclerView mGridList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
